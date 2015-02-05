4270642b3fb11d3b9076ccb91a59c269a7c1bdca
dasis <dianneasis@gmail.com>
2015-02-04 16:36:44 -0800

    Create gh-pages branch via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,1 @@
-angular.module('myApp', ['angular-confirm'])
-  .controller('MyController', function($scope, $confirm) {
-
-    $scope['delete'] = function() {
-      $scope.deleted = 'Deleted';
-    };
-    
-    $scope['deleteConfirm'] = function() {
-      $confirm({text: 'Are you sure you want to delete?'})
-        .then(function() {
-          $scope.deletedConfirm = 'Deleted';
-        });
-    };
-    
-  });
+console.log('This would be the main JS file.');
